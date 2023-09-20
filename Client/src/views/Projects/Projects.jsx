3cb0f2530dad854ef7895769bde4b2b6120f5d32
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-09-19 21:19:01 -0300

    css projects

--- Client/src/views/Projects/Projects.jsx
+++ Client/src/views/Projects/Projects.jsx
@@ -3,6 +3,9 @@ import style from './Project.module.css';
 
 const Projects = () => {
 
+	const handleProjectClick = (url) => {
+		window.location.href = url
+	}
 
 	return (
 		<>
@@ -10,11 +13,28 @@ const Projects = () => {
 				<div className={style.title}>
 					<h1>My Projects</h1>
 				</div>
-				<div className={style.cardP}>
-					<img src="/projects/Portada-Patitas.png" alt="Patitas sin Hogar" />
-					<img src="/projects/portada-countries.png" alt="PI Countries" />
-					<img src="/projects/icono+.png" alt="Próximamente..." />
-					<img src="/projects/icono+.png" alt="Próximamente..." />
+				<div className={style.imgContainer}>
+					<div className={style.cardP}>
+						<img src="/projects/Portada-Patitas.png" alt="Patitas sin Hogar" onClick={() => handleProjectClick("https://henry-pf-git-main-rieraandres.vercel.app/")}/>
+						<span>Patitas sin Hogar</span>
+					</div>
+					<div className={style.cardP}>
+						<img src="/projects/portada-countries.png" alt="PI Countries" onClick={() => handleProjectClick("https://github.com/MarcosMoreno0912/PI_Countries_Marcos-")} />
+						<span>Countries</span>
+					</div>
+					<div className={style.cardP}>
+						<img src="/projects/icono+.png" alt="Próximamente..." />
+						<span>Próximamente...</span>
+					</div>
+					<div className={style.cardP}>
+						<img src="/projects/icono+.png" alt="Próximamente..." />
+				  	<span>Próximamente...</span>
+					</div>
+					<div className={style.cardP}>
+						<img src="/projects/icono+.png" alt="Próximamente..." />
+				  	<span>Próximamente...</span>
+					</div>
+
 				</div>
 			</div>
 		</>
