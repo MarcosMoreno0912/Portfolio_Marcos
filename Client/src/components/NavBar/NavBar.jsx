0199a5e93cba884c96df08f2ab9a36f1e6313d36
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-09-29 21:55:11 -0300

    Home Responsive css

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import style from './NavBar.module.css';
 
 const NavBar = () => {
 	const navigate = useNavigate()
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+	const toggleMobileMenu = () => {
+		setMobileMenuOpen(!mobileMenuOpen);
+	};
 
 	const handleHomeClick = () => {
 		navigate('/')
@@ -27,34 +32,50 @@ const NavBar = () => {
 
 	return (
 		<div className={style.nav} >
-			
 			<img src="../../../pestañaPortfolio.jpg" alt="Logo" onClick={handleHomeClick}/>
+			
+			<button className={style.menuIcon} onClick={toggleMobileMenu}>
+        <div className={style.bar}></div>
+        <div className={style.bar}></div>
+        <div className={style.bar}></div>
+      </button>
 
-			<button>
+      {/* Contenedor del menú móvil */}
+      {mobileMenuOpen && (
+        <nav className={style.mobileMenu}>
+          <button onClick={handleHomeClick}>Home</button>
+          <button onClick={handleAboutClick}>About Me</button>
+          <button onClick={handleTechSkillsClick}>Tech Skills</button>
+    			<button onClick={handleProjectsClick}>Projects</button>
+    			<button onClick={handleContactMeClick}>Contact Me</button>
+        </nav>
+      )}
+
+			<button className={style.navBtn}>
     		<span className={style.box} onClick={handleHomeClick}>
         	Home
     		</span>
 			</button>
 
-			<button>
+			<button className={style.navBtn}>
     		<span className={style.box} onClick={handleAboutClick}>
         	About Me
     		</span>
 			</button>
 
-			<button>
+			<button className={style.navBtn}>
     		<span className={style.box} onClick={handleTechSkillsClick}>
         	Tech Skills
     		</span>
 			</button>
 
-			<button>
+			<button className={style.navBtn}>
     		<span className={style.box} onClick={handleProjectsClick}>
         	Projects
     		</span>
 			</button>
 
-			<button>
+			<button className={style.navBtn}>
     		<span className={style.box} onClick={handleContactMeClick}>
         	Contact Me
     		</span>
